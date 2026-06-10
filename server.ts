f0ab46cc8a1d08716a4d67439fcb8c88f14bd94c
John Champion <johnt.champion@gmail.com>
2026-06-10 08:41:27 -0400

    fix dirname

--- server.ts
+++ server.ts
@@ -6,8 +6,8 @@ const app = express()
 const PORT = process.env.PORT ?? 3000
 
 app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'))
-app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(process.cwd(), 'views'))
+app.use(express.static(path.join(process.cwd(), 'public')))
 
 app.get('/', (req: Request, res: Response) => {
   const today = new Date()
